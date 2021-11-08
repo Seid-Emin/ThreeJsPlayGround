3a739b9a21c364d11cc24f9a8b6880c35bbb166e
Seid <seid.saydahmed@gmail.com>
2021-11-08 11:35:13 +0200

    global clipping

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,36 @@
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as dat from 'dat.gui';
 
 
 export const App = () => {
-    let camera, scene, renderer;
+    let camera, scene, renderer, gui;
 
     const params = {
-        clipIntersection: true,
-        planeConstant: {
-            x: 1,
-            y: -1,
-            z: -1
-        },
-        showHelpers: true,
+        clipIntersection: true, planeConstant: {
+            x1: 1, y1: 1, z1: 1, x2: 1, y2: 1, z2: 1,
+        }, showHelpers: true,
     };
 
     const clipPlanes = [
-        new THREE.Plane(new THREE.Vector3(1, 0, 0), params.planeConstant.x),
-        new THREE.Plane(new THREE.Vector3(0, -1, 0), params.planeConstant.x),
-        new THREE.Plane(new THREE.Vector3(0, 0, -1), params.planeConstant.x),
-        // new THREE.Plane(new THREE.Vector3(0, 0, 1), -1),
-        // new THREE.Plane(new THREE.Vector3(0, 1, 0), -1),
-        // new THREE.Plane(new THREE.Vector3(1, 0, 0), -1),
+        new THREE.Plane(new THREE.Vector3(-1, 0, 0), params.planeConstant.x1),
+        new THREE.Plane(new THREE.Vector3(0, -1, 0), params.planeConstant.y1),
+        new THREE.Plane(new THREE.Vector3(0, 0, -1), params.planeConstant.z1),
+        new THREE.Plane(new THREE.Vector3(0, 0, 1), params.planeConstant.z2),
+        new THREE.Plane(new THREE.Vector3(0, 1, 0), params.planeConstant.y2),
+        new THREE.Plane(new THREE.Vector3(1, 0, 0), params.planeConstant.x2)
     ];
 
-    useEffect(() => {
-        init();
-        render();
-    }, []);
-
     function init() {
-        renderer = new THREE.WebGLRenderer({antialias: true});
+        renderer = new THREE.WebGLRenderer({ antialias: true });
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.localClippingEnabled = true;
+        renderer.clippingPlanes = clipPlanes;
+        renderer.clipIntersection = true;
         document.body.appendChild(renderer.domElement);
 
         scene = new THREE.Scene();
@@ -59,10 +52,8 @@ export const App = () => {
         for (let i = 1; i <= 30; i += 2) {
             const geometry = new THREE.SphereGeometry(i / 30, 48, 24);
             const material = new THREE.MeshLambertMaterial({
-                color: new THREE.Color().setHSL(Math.random(), 0.5, 0.5),
-                side: THREE.DoubleSide,
-                clippingPlanes: clipPlanes,
-                clipIntersection: params.clipIntersection,
+                color: new THREE.Color().setHSL(Math.random(), 0.5, 0.5), side: THREE.DoubleSide, // clippingPlanes: clipPlanes,
+                // clipIntersection: params.clipIntersection,
             });
 
             group.add(new THREE.Mesh(geometry, material));
@@ -75,14 +66,14 @@ export const App = () => {
         helpers.add(new THREE.PlaneHelper(clipPlanes[0], 2, 0xff0000));
         helpers.add(new THREE.PlaneHelper(clipPlanes[1], 2, 0x00ff00));
         helpers.add(new THREE.PlaneHelper(clipPlanes[2], 2, 0x0000ff));
-        // helpers.add(new THREE.PlaneHelper(clipPlanes[3], 2, 0x00FFFF));
-        // helpers.add(new THREE.PlaneHelper(clipPlanes[4], 2, 0x008080));
-        // helpers.add(new THREE.PlaneHelper(clipPlanes[5], 2, 0x000080));
+        helpers.add(new THREE.PlaneHelper(clipPlanes[3], 2, 0x00FFFF));
+        helpers.add(new THREE.PlaneHelper(clipPlanes[4], 2, 0x008080));
+        helpers.add(new THREE.PlaneHelper(clipPlanes[5], 2, 0x000080));
         helpers.visible = true;
         scene.add(helpers);
 
         // gui
-        const gui = new dat.GUI();
+        gui = new dat.GUI();
 
         gui.add(params, 'clipIntersection').name('clip intersection').onChange(function (value) {
             const children = group.children;
@@ -101,48 +92,36 @@ export const App = () => {
         //     render();
         // });
 
-        gui.add(params.planeConstant, 'x', -1, 1).step(0.01).name('plane constant x1 ').onChange(function (value) {
-            // for (let j = 0; j < clipPlanes.length; j++) {
-                clipPlanes[0].constant = value;
-            // }
+        gui.add(params.planeConstant, 'x1', -1, 1).step(0.01).name('plane constant x1 ').onChange(function (value) {
+            clipPlanes[0].constant = value;
 
             render();
         });
-        gui.add(params.planeConstant, 'y', -1, 1).step(0.01).name('plane constant y1').onChange(function (value) {
-            // for (let j = 0; j < clipPlanes.length; j++) {
-                clipPlanes[1].constant = value;
-            // }
+        gui.add(params.planeConstant, 'y1', -1, 1).step(0.01).name('plane constant y1').onChange(function (value) {
+            clipPlanes[1].constant = value;
 
             render();
         });
-        gui.add(params.planeConstant, 'z', -1, 1).step(0.01).name('plane constant z1').onChange(function (value) {
-            // for (let j = 0; j < clipPlanes.length; j++) {
-                clipPlanes[2].constant = value;
-            // }
+        gui.add(params.planeConstant, 'z1', -1, 1).step(0.01).name('plane constant z1').onChange(function (value) {
+            clipPlanes[2].constant = value;
 
             render();
         });
-        // gui.add(params, 'planeConstant', -1, 1).step(0.01).name('plane constant z2').onChange(function (value) {
-        //     // for (let j = 0; j < clipPlanes.length; j++) {
-        //         clipPlanes[3].constant = value;
-        //     // }
-        //
-        //     render();
-        // });
-        // gui.add(params, 'planeConstant', -1, 1).step(0.01).name('plane constant y2').onChange(function (value) {
-        //     // for (let j = 0; j < clipPlanes.length; j++) {
-        //         clipPlanes[4].constant = value;
-        //     // }
-        //
-        //     render();
-        // });
-        // gui.add(params, 'planeConstant', -1, 1).step(0.01).name('plane constant x2').onChange(function (value) {
-        //     // for (let j = 0; j < clipPlanes.length; j++) {
-        //         clipPlanes[5].constant = value;
-        //     // }
-        //
-        //     render();
-        // });
+        gui.add(params.planeConstant, 'z2', -1, 1).step(0.01).name('plane constant z2').onChange(function (value) {
+            clipPlanes[3].constant = value;
+
+            render();
+        });
+        gui.add(params.planeConstant, 'y2', -1, 1).step(0.01).name('plane constant y2').onChange(function (value) {
+            clipPlanes[4].constant = value;
+
+            render();
+        });
+        gui.add(params.planeConstant, 'x2', -1, 1).step(0.01).name('plane constant x2').onChange(function (value) {
+            clipPlanes[5].constant = value;
+
+            render();
+        });
 
         gui.add(params, 'showHelpers').name('show helpers').onChange(function (value) {
             helpers.visible = value;
@@ -151,7 +130,6 @@ export const App = () => {
         });
 
         window.addEventListener('resize', onWindowResize);
-
     }
 
     function onWindowResize() {
@@ -163,8 +141,20 @@ export const App = () => {
     }
 
     function render() {
+        for (let i in gui.__controllers) {
+            gui.__controllers[i].updateDisplay();
+        }
+
+
         renderer.render(scene, camera);
     }
+
+    useEffect(() => {
+        init();
+        render();
+
+        // return () => document.body.removeChild(rendered.domElement);
+    }, []);
 
 
     return <div className="App"/>;
